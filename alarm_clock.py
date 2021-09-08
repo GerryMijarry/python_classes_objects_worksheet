ad1b09beaf35ea382a56ccd92ecdd9b513a31ad1
GerryMijarry <89660119+GerryMijarry@users.noreply.github.com>
2021-09-08 12:30:21 -0600

    cleaned up print statements

--- alarm_clock.py
+++ alarm_clock.py
@@ -12,7 +12,7 @@ class AlarmClock:
     def turn_on_off(self, requested_state):
         self.on_off = requested_state
 
-        print("The alarm is set to " + str(self.on_off))
+        print("The alarm is set to " + str(self.on_off) + ". Dont forget to turn it on!")
 
     def set_alarm(self, new_time):
         self.alarm_time = new_time
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from alarm_clock import AlarmClock
 from datetime import datetime
 
-# Done - As a developer, I want to use Python’s proper snake_case for variable names.
-# Done - As a developer, I want to create a AlarmClock class.
-# Done - As a developer, I want the AlarmClock class to have class properties to keep track of the AlarmClock’s current time, whether the alarm is on or off, and the time the alarm is set to. (You can use arbitrary strings to represent the time, it does not need to accurately tell the current time or change over time).
-# Done - As a developer, I want the AlarmClock class to have a method to set (or change) the current time and print to the console the current time.
-# Done - As a developer, I want the AlarmClock class to have a method to toggle the alarm on or off. 
-# Done - As a developer, I want the AlarmClock class to have a method to set the current alarm time and print to the console the current alarm time.
-# Done - As a developer, I want to import the AlarmClock class into main.py so I can instantiate it as a new AlarmClock object and call methods on it.
+# * Done - As a developer, I want to use Python’s proper snake_case for variable names.
+# * Done - As a developer, I want to create a AlarmClock class.
+# * Done - As a developer, I want the AlarmClock class to have class properties to keep track of the AlarmClock’s current time, whether the alarm is on or off, and the time the alarm is set to. (You can use arbitrary strings to represent the time, it does not need to accurately tell the current time or change over time).
+# * Done - As a developer, I want the AlarmClock class to have a method to set (or change) the current time and print to the console the current time.
+# * Done - As a developer, I want the AlarmClock class to have a method to toggle the alarm on or off. 
+# * Done - As a developer, I want the AlarmClock class to have a method to set the current alarm time and print to the console the current alarm time.
+# * Done - As a developer, I want to import the AlarmClock class into main.py so I can instantiate it as a new AlarmClock object and call methods on it.
 
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
@@ -17,6 +17,8 @@ ac1 = AlarmClock(current_time)
 
 ac1.set_alarm("00:06:30")
 ac1.turn_on_off(True)
+
+ac1.change_time("00:11:59")
 
 
 
